ec1ebe1eda8f1089173a5c05c64e910b75dfdad5
Sudipta <sudiptaghosh9136@gmail.com>
2025-08-23 14:51:59 +0530

    modify service provider

--- src/UserInterfaceServiceProvider.php
+++ src/UserInterfaceServiceProvider.php
@@ -8,17 +8,17 @@ class UserInterfaceServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/masterdata.php', 'masterdata');
+        $this->mergeConfigFrom(__DIR__ . '/../config/userinterface.php', 'userinterface');
     }
 
     public function boot(): void
     {
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'masterdata');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'userinterface');
 
         $this->publishes([
-            __DIR__ . '/../config/masterdata.php' => config_path('masterdata.php'),
-        ], 'masterdata-config');
+            __DIR__ . '/../config/userinterface.php' => config_path('userinterface.php'),
+        ], 'userinterface-config');
     }
 }
